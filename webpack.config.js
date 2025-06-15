e931cdf0256fee5c335b06542f27d149b9b39aba
Adam Fortune <adamfortune96@protonmail.com>
2025-06-15 11:49:22 -0500

    Fix an issue with webpack not having access to some crypto functions

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const path = require('path');
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
 
 /**@type {import('webpack').Configuration}*/
 const config = {
